ea3af3f7d14d56113939b463379313cbcc5c9ccd
pheno <nicolas.enault@gmail.com>
2020-09-27 18:58:35 +0200

    request to spotify API implemented

---
+++ config/mongo.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+mongoose.connection.on("connected", () =>
+  console.log("yay mongodb connected :)")
+);
+
+mongoose.connection.on("error", () =>
+  console.log("nay db connexion error sorry :(")
+);
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Rabbit music" });
 });
+
+router.get("/song-search", function (req, res, next) {
+  axios({
+    url: "https://accounts.spotify.com/api/token",
+    method: "post",
+    params: {
+      grant_type: "client_credentials",
+    },
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    auth: {
+      username: process.env.CLIENT_ID,
+      password: process.env.CLIENT_SECRET,
+    },
+  })
+    .then(function (response) {
+      // console.log(response.data.access_token);
+      const accessToken = response.data.access_token;
+      const refreshToken = response.data.refresh_token;
+
+      axios({
+        url: `https://api.spotify.com/v1/search?q=${req.query.song}&type=track`,
+        
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        params: {
+          access_token: accessToken,
+          refresh_token: refreshToken,
+        },
+      })
+        .then((response) => {
+          console.log(response.data.tracks.items);
+          res.render("song-search-results", { results: response.data.tracks.items});
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch(function (error) {});
+});
+
 
 module.exports = router;
