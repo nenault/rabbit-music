40b3b1b3ca9cec612ec5f8a20d6ea70313aecaec
JorellLangmar <jorell.langmar@gmail.com>
2020-09-28 10:10:09 +0200

    Starting Navbar

--- config/mongo.js
+++ config/mongo.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 mongoose.connection.on("connected", () =>
