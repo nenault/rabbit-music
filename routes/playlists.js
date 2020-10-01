aa2aa46f29ef67e7b0b3751d05beb35d856b3dd2
pheno <nicolas.enault@gmail.com>
2020-10-01 22:40:46 +0200

    export songs in playlist

--- routes/playlists.js
+++ routes/playlists.js
@@ -741,6 +741,8 @@ router.get("/export/:id", protectPrivateRoute, async (req, res, next) => {
     const playlist = await Playlist.findById(playlistId).populate("user");
     const spotiId = playlist.user.spotifyid;
 
+    req.session.playlistId = playlistId;
+
     const client_id = process.env.CLIENT_ID;
     const client_secret = process.env.CLIENT_SECRET;
     const redirect_uri =
@@ -768,9 +770,19 @@ router.get(
   async (req, res, next) => {
     try {
       let code = req.query.code;
-      let spotiId = req.session.currentUser.spotifyid;
-      //const playlistId = req.params.id;
-      // console.log(playlistId);
+     
+      const playlist = await Playlist.findById(req.session.playlistId).populate(
+        "user"
+      );
+
+      playlist.songs = playlist.songs.map((i) => "spotify:track:" + i);
+
+      const uriString = playlist.songs.toString();
+
+      const data = {
+        name: playlist.name,
+        user: playlist.user.spotifyid,
+      };
 
       const client_id = process.env.CLIENT_ID;
       const client_secret = process.env.CLIENT_SECRET;
@@ -800,8 +812,8 @@ router.get(
           const accessToken = response.data.access_token;
           const refreshToken = response.data.refresh_token;
 
-          /*  axios({
-            url: `https://api.spotify.com/v1/users/${spotiId}/playlists`,
+          axios({
+            url: `https://api.spotify.com/v1/users/${data.user}/playlists`,
             method: "post",
             headers: {
               Accept: "application/json",
@@ -812,16 +824,36 @@ router.get(
               refresh_token: refreshToken,
             },
             data: {
-              name: "JORELLLLLLL",
+              name: data.name,
             },
           })
             .then((response) => {
-              console.log(response.data);
-              //res.send(response.data.tracks.items);
+              const newPlaylistId = response.data.id;
+              axios({
+                url: `https://api.spotify.com/v1/playlists/${newPlaylistId}/tracks`,
+                method: "post",
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                },
+                params: {
+                  access_token: accessToken,
+                  refresh_token: refreshToken,
+                },
+                data: {
+                  uris: playlist.songs,
+                },
+              })
+                .then((response) => {
+                  res.redirect(`/playlists/${playlist.id}`);
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
             })
             .catch((err) => {
               console.log(err);
-            }); */
+            });
         })
         .catch(function (error) {});
     } catch (error) {
