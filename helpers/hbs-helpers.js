055c4d7e8ed08a1f8e4c523cbbfbb9e7654120b7
pheno <nicolas.enault@gmail.com>
2020-10-01 12:41:43 +0200

    add spotify id

--- helpers/hbs-helpers.js
+++ helpers/hbs-helpers.js
@@ -14,6 +14,25 @@ hbs.registerHelper("imageDisplay", function (value1) {
   return image
 })
 
+hbs.registerHelper("artistDisplay", function (list) {
+  if (list.length > 1) {
+    const artArr = [];
+    for (let i = 0; i < list.length; i++) {
+      artArr.push(list[i]);
+    }
+    const artistsList = artArr.join(", ");
+    return artistsList;
+  }
+})
 
-
-
+/* if (song.artists.length > 1) {
+  const artArr = [];
+  for (let i = 0; i < song.artists.length; i++) {
+    artArr.push(song.artists[i].name);
+  }
+  // console.log(artArr);
+  let getPreview = "";
+  if (song.preview_url != null) {
+    getPreview = song.preview_url;
+  }
+  const artistsList = artArr.join(", "); */
